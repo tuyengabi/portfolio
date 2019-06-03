6a9919a57a102b33ff3f7acf75b0f6ba2e65cd22
tuyengabi <tuyen.gabi@gmail.com>
2019-06-04 00:13:15 +0200

    add file Contentabout.js

---
+++ src/components/Contentabout.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Contentabout extends Component {
+  render() {
+    return  (
+      <div>
+        <h1>
+          Tuyen Le
+        </h1>
+        <h3>
+          DÉVELOPPEUSE FRONT-END <br> 
+          DESIGNER
+        </h3>
+        <p>
+        dhgfjgk hkjhkjklj hjkjl hjkhlkj hjkjhk jhkjhl jhgkjhlj jhgkhl jhgkjhl jhgkjhkl jgkhl kjhlh khlhl khlj khljm khljm khlhl khkl jgfhfhn sqhgj hdskj riuyr kjhglk vc jgl rdijl qgds evdco kujvlksb qij hckl cbv.
+        </p>
+      </div>
+    )
+    
+  }
+}
+
+export default Contentabout;
