c6c53e01efc7b552b64fee9bcecd3acb8ac06dd3
tuyengabi <tuyen.gabi@gmail.com>
2019-06-04 02:15:45 +0200

    add file Contentexperience.js

---
+++ src/components/Contentexpenrience.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Experience extends Component {
+  render() {
+    return  (
+      <div>
+        <h3>EXPÉRIENCES</h3>
+        <div>
+          <h4>2014 - 08/2018</h4>
+          <p>
+            Serveuse de restaurant <br> 
+            Prothésiste ongulaire - Paris - France.
+          </p>
+          <p>
+            Employée Polyvalente Hotel Le Tadorne <br>
+            Ouvrières d'agricole - Aube - France.
+          </p>
+        </div>
+        <div>
+          <h4>2009 - 2013</h4>
+          <p>
+            Chef Designer de mode société NMN <br>
+            secteur D'industrie de Textile et Uniform - Hochiminh - Vietnam
+          </p>
+          <h4>2007 - 2009</h4>
+          <p>
+            Designer de mode société TAS <br> 
+            secteur Événementiel - Hochiminh - Vietnam
+          </p>
+        </div>
+        <h3>PROJETS</h3>
+        <div>
+          <h4>Converlience</h4>
+          <a><img /></a>
+        </div>
+        <div>
+          <h4>LoSfizio</h4>
+          <a><img /></a>
+        </div>
+      </div>
+    )
+    
+  }
+}
+
+export default Experience;
