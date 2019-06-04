7f43d492eeabc355c0f3f2c7f307f5b76edebe40
tuyengabi <tuyen.gabi@gmail.com>
2019-06-04 14:45:42 +0200

    install sass, and change name App.css index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import About from './components/About.js'
 import Skill from './components/Skill.js'
 import Experience from './components/Experience.js'
