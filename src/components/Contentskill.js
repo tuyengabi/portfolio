1da3c971dc7dfc7898e2c32cbbea3c712c699543
tuyengabi <tuyen.gabi@gmail.com>
2019-06-04 11:52:33 +0200

    add file Contentskill.js

---
+++ src/components/Contentskill.js
@@ -0,0 +1,70 @@
+ 
+import React, { Component } from 'react';
+
+class Contentskill extends Component {
+  render() {
+    return  (
+      <div>
+        <div>
+          <ul>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+            <li>
+              <img />
+              <img />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Travail en équipe:</h3>
+          <p>Facilité à s'intégrer dans une nouvelle équipe.</p>
+          <h3>Autonomie:</h3>
+          <p>Prendre en charge son activité sans devoir être encadré de façon continue.</p>
+          <h3>Force de proposition:</h3>
+          <p>Grâce à mon analyse et mon écoute, je suis force de proposition pour innover dans de nouvelles idées.</p>
+          <h3>Prise de recul:</h3>
+          <p>Je peux faire preuve d'objectivité, à prendre de la distance pour analyser les faits, les situations et les interactions avant d'agir ou de prendre une décision.</p>
+          <h3>Kha nang soi chi do:</h3>
+          <p>Définir un concept créatif en fonction du support choisi.</p>          
+        </div>
+      </div>
+    )
+    
+  }
+}
+
+export default Contentskill;
