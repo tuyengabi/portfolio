be3c1ac7cc2f1994b62f0bfafbd56f7f770763f1
tuyengabi <tuyen.gabi@gmail.com>
2019-06-06 11:13:17 +0200

    modify ContentSkill.js , add ContentSkill.scss

--- src/components/ContentSkill.js
+++ src/components/ContentSkill.js
@@ -1,65 +1,57 @@
- 
 import React, { Component } from 'react';
+import './css/ContentSkill.scss';
 
 class ContentSkill extends Component {
   render() {
     return  (
-      <div>
-        <div>
-          <ul>
+      <div className="contentSkill">
+        <div className="tool">
+          <ul className="toolDev">
             <li>
-              <img />
               <img />
             </li>
             <li>
               <img />
+            </li>
+            <li>
               <img />
             </li>
             <li>
               <img />
+            </li>
+            <li>
+              <img />
+            </li>
+          </ul>
+          <ul className="toolDes">
+            <li>
               <img />
             </li>
             <li>
               <img />
+            </li>
+            <li>
               <img />
             </li>
             <li>
               <img />
-              <img />
             </li>
             <li>
-              <img />
-              <img />
-            </li>
-            <li>
-              <img />
-              <img />
-            </li>
-            <li>
-              <img />
-              <img />
-            </li>
-            <li>
-              <img />
-              <img />
-            </li>
-            <li>
-              <img />
               <img />
             </li>
           </ul>
         </div>
-        <div>
-          <h3>Travail en équipe:</h3>
-          <p>Facilité à s'intégrer dans une nouvelle équipe.</p>
-          <h3>Autonomie:</h3>
-          <p>Prendre en charge son activité sans devoir être encadré de façon continue.</p>
-          <h3>Force de proposition:</h3>
-          <p>Grâce à mon analyse et mon écoute, je suis force de proposition pour innover dans de nouvelles idées.</p>
-          <h3>Prise de recul:</h3>
-          <p>Je peux faire preuve d'objectivité, à prendre de la distance pour analyser les faits, les situations et les interactions avant d'agir ou de prendre une décision.</p>
-          <h3>Kha nang soi chi do:</h3>
-          <p>Définir un concept créatif en fonction du support choisi.</p>          
+        <div className="skill">
+          <h3 className="skillTitle">Travail en équipe:</h3>
+          <p className="skillDetail">Facilité à s'intégrer dans une nouvelle équipe.</p>
+          <h3 className="skillTitle">Autonomie:</h3>
+          <p className="skillDetail">Prendre en charge son activité sans devoir être encadré de façon continue.</p>
+          <h3 className="skillTitle">Force de proposition:</h3>
+          <p className="skillDetail">Grâce à mon analyse et mon écoute, je suis force de proposition pour innover dans de nouvelles idées.</p>
+          <h3 className="skillTitle">Prise de recul:</h3>
+          <p className="skillDetail">Je peux faire preuve d'objectivité, à prendre de la distance pour analyser les faits, les situations et les interactions avant d'agir ou de prendre une décision.</p>
+          <h3 className="skillTitle">Vonlonté d'apprendre:</h3>
+          <p className="skillDetail">Définir un concept créatif en fonction du support choisi.</p>          
         </div>
       </div>
     )
