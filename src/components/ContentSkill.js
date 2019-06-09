28e0ebe1cfa7bf0f40d24d1864ed5da0d10f73ff
tuyengabi <tuyen.gabi@gmail.com>
2019-06-09 21:41:01 +0200

    fix className in class quality ContentSkill.js, add attribute center-middle in file ContentSkill.scss

--- src/components/ContentSkill.js
+++ src/components/ContentSkill.js
@@ -15,51 +15,51 @@ class ContentSkill extends Component {
   render() {
     return  (
       <section className="contentSkill">
-        <div className="tool">
-          <h2 className="title-tool">Atouts</h2>
-          <figure className="html">
+        <div className="tool center-middle">
+          <h2 className="toolTitle center-middle">Atouts</h2>
+          <figure className="html center-middle">
             <img className="logo" src={ htmlLogo }/>
           </figure>
-          <figure className="css">
+          <figure className="css center-middle">
             <img className="logo" src={ cssLogo } />
           </figure>
-          <figure className="js">
+          <figure className="js center-middle">
             <img className="logo" src={ jsLogo } />
           </figure>
-          <figure className="vuejs">
+          <figure className="vuejs center-middle">
             <img className="logo" src={ vuejsLogo } />
           </figure>
-          <figure className="reactjs">
+          <figure className="reactjs center-middle">
             <img className="logo" src={ reactjsLogo } />
           </figure>
-          <figure className="vim">
+          <figure className="vim center-middle">
             <img className="logo" src={ vimLogo } />
           </figure>
-          <figure className="ai">
+          <figure className="ai center-middle">
             <img className="logo" src={ aiLogo } />
           </figure>
-          <figure className="ps">
+          <figure className="ps center-middle">
             <img className="logo" src={ psLogo } />
           </figure>
-          <figure className="draw">
+          <figure className="draw center-middle">
             <img className="logo" src={ drawLogo } />
           </figure>
-          <figure className="camera">
+          <figure className="camera center-middle">
             <img className="logo" src={ cameraLogo } />
           </figure>
         </div>
-        <div className="quafigurety">
-          <h2 className="">Compétences</h2>
-          <h3 className="quafiguretyTitle">Travail en équipe:</h3>
-          <p className="skillDetail">Facifigureté à s'intégrer dans une nouvelle équipe.</p>
-          <h3 className="quafiguretyTitle">Autonomie:</h3>
-          <p className="skillDetail">Prendre en charge son activité sans devoir être encadré de façon continue.</p>
-          <h3 className="quafiguretyTitle">Force de proposition:</h3>
-          <p className="skillDetail">Grâce à mon analyse et mon écoute, je suis force de proposition pour innover dans de nouvelles idées.</p>
-          <h3 className="quafiguretyTitle">Prise de recul:</h3>
-          <p className="skillDetail">Je peux faire preuve d'objectivité, à prendre de la distance pour analyser les faits, les situations et les interactions avant d'agir ou de prendre une décision.</p>
-          <h3 className="quafiguretyTitle">Vonlonté d'apprendre:</h3>
-          <p className="skillDetail">Définir un concept créatif en fonction du support choisi.</p>          
+        <div className="quality center-middle">
+          <h2 className="qualityTitle center-middle">Compétences</h2>
+          <h3 className="qualityList">Travail en équipe:</h3>
+          <p className="qualityDetail">Facifigureté à s'intégrer dans une nouvelle équipe.</p>
+          <h3 className="qualityList">Autonomie:</h3>
+          <p className="qualityDetail">Prendre en charge son activité sans devoir être encadré de façon continue.</p>
+          <h3 className="qualityList">Force de proposition:</h3>
+          <p className="qualityDetail">Grâce à mon analyse et mon écoute, je suis force de proposition pour innover dans de nouvelles idées.</p>
+          <h3 className="qualityList">Prise de recul:</h3>
+          <p className="qualityDetail">Je peux faire preuve d'objectivité, à prendre de la distance pour analyser les faits, les situations et les interactions avant d'agir ou de prendre une décision.</p>
+          <h3 className="qualityList">Vonlonté d'apprendre:</h3>
+          <p className="qualityDetail">Définir un concept créatif en fonction du support choisi.</p>          
         </div>
       </section>
     )
