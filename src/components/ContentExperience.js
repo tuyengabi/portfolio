80e04ec9a2d14e1461a35b31a9155ed1f1230681
tuyengabi <tuyen.gabi@gmail.com>
2019-06-10 11:21:56 +0200

    add className in file ContentExperience.js

--- src/components/ContentExperience.js
+++ src/components/ContentExperience.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class ContentExperience extends Component {
   render() {
     return  (
-      <section>
-        <h3>EXPÉRIENCES</h3>
-        <div>
-          <h4>2014 - 08/2018</h4>
+      <section className="ContentExperience">
+        <h3 className="experienceTitle">EXPÉRIENCES</h3>
+        <div className="experience">
+          <h4 className="year">2014 - 08/2018</h4>
           <p>
             Serveuse de restaurant <br /> 
             Prothésiste ongulaire - Paris - France.
@@ -15,28 +15,26 @@ class ContentExperience extends Component {
             Employée Polyvalente Hotel Le Tadorne <br />
             Ouvrières d'agricole - Aube - France.
           </p>
-        </div>
-        <div>
-          <h4>2009 - 2013</h4>
+          <h4 className="year">2009 - 2013</h4>
           <p>
             Chef Designer de mode société NMN <br />
             secteur D'industrie de Textile et Uniform - Hochiminh - Vietnam
           </p>
-          <h4>2007 - 2009</h4>
+          <h4 className="year">2007 - 2009</h4>
           <p>
             Designer de mode société TAS <br /> 
             secteur Événementiel - Hochiminh - Vietnam
           </p>
         </div>
-        <h3>PROJETS</h3>
-        <div>
-          <h4>Converlience</h4>
-          <a><img /></a>
-        </div>
-        <div>
-          <h4>LoSfizio</h4>
-          <a><img /></a>
-        </div>
+        <h3 className="projectTitle">PROJETS</h3>
+        <figure className="project">
+          <h4 className="projectName">Converlience</h4>
+          <a><img className="projectLogo" src={  } /></a>
+          <h4 className="projectName">LoSfizio</h4>
+          <a><img className="projectLogo" src={  } /></a>
+          <h4 className="projectName">Voir plus sur mon Github:</h4>
+          <a><img className="projectLogo" src={  } /></a>
+        </figure>
       </section>
     )
     
